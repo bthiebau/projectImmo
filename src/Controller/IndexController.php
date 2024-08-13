d95e50405f744ced532ae1c96c49fd7660222fea
bthiebau <bastien.thiebaut@hotmail.com>
2024-08-13 14:03:05 +0200

    Initialisation du projet
    - création de l'IndexController avec ses routes pour les pages standards
    
    - création des templates pour les pages génériques

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'home_page')]
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/about', name: 'about_page')]
+    public function about(): Response
+    {
+        return $this->render('index/about.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact_page')]
+    public function contact(): Response
+    {
+        return $this->render('index/contact.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+
+}
