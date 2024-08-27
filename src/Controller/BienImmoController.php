d9dddfd3552d96f74859e55db5cd6b2060066540
bthiebau <bastien.thiebaut@hotmail.com>
2024-08-27 18:55:08 +0200

    Ajout de la fonctionalité de tri des resultats

--- src/Controller/BienImmoController.php
+++ src/Controller/BienImmoController.php
@@ -5,17 +5,35 @@ namespace App\Controller;
 use App\Entity\BienImmo;
 use App\Repository\BienImmoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BienImmoController extends AbstractController
 {
+    // #[Route('/properties', name: 'properties_list')]
+    // public function list(BienImmoRepository $bienImmoRepository): Response
+    // {
+    //     $properties = $bienImmoRepository->findAll();
+    //     return $this->render('bien_immo/list.html.twig', [
+    //         'properties' => $properties,
+    //     ]);
+    // }
+
     #[Route('/properties', name: 'properties_list')]
-    public function list(BienImmoRepository $bienImmoRepository): Response
+    public function list(BienImmoRepository $bienImmoRepository, Request $request): Response
     {
-        $properties = $bienImmoRepository->findAll();
+        // Récupération des paramètres de tri depuis la requête
+        $sortField = $request->query->get('sort', 'price'); // Par défaut, on trie par prix
+        $sortOrder = $request->query->get('order', 'asc');  // Par défaut, ordre croissant
+
+        // Recherche avec tri
+        $properties = $bienImmoRepository->findBy([], [$sortField => $sortOrder]);
+
         return $this->render('bien_immo/list.html.twig', [
             'properties' => $properties,
+            'sortField' => $sortField,
+            'sortOrder' => $sortOrder,
         ]);
     }
 
