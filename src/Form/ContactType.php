1a95bcefda50c8127c6e107b157340a090d66d77
bthiebau <bastien.thiebaut@hotmail.com>
2024-09-02 09:11:50 +0200

    ajout label contactform

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -6,6 +6,7 @@ use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,9 +16,11 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
             ->add('email', EmailType::class)
-            ->add('message', TextType::class)
+            ->add('message', TextareaType::class)
             ->add("Envoyer", SubmitType::class)
         ;
     }
